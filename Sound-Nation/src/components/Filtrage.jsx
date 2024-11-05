ec2e48fda08ef10c07da0d96965df1e115ef6e2c
Sébastien Lotten <sebastienlotten@proton.me>
2024-11-05 12:09:19 +0100

    WIP En savoir plus

--- Sound-Nation/src/components/Filtrage.jsx
+++ Sound-Nation/src/components/Filtrage.jsx
@@ -1,71 +1,30 @@
 import { useContext, useEffect, useState } from "react";
-import { FilterContext } from "../contexts/FilterContext";
-import { getDataFilters } from "../utils/dataFilters";
-import { CheckBoxTest } from "./CheckBoxTest";
-
-export const Filtrage = () => {
-  const { dataFilters, setDataFilters } = useContext(FilterContext);
+import { CheckBox } from "./CheckBox";
+// Composant qui gère le filtrage des groupes par date, heure et scène
+export const Filtrage = ({uniqueDate, uniqueScene, uniqueHour, setDataFilter, dataFilter, isChecked, setIsChecked}) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [allChecked, setAllChecked] = useState(true);
-  const [filtersNumber, setFiltersNumber] = useState(0);
 
-  useEffect(() => {
-    const filters = getDataFilters();
-    const sum =
-      filters.Jours.length + filters.Scenes.length + filters.Heures.length;
-    setDataFilters(filters);
-    setFiltersNumber(sum);
-  }, []);
-
-  useEffect(() => {
-    const storedData = localStorage.getItem("selectedCheckBox");
-    if (storedData) {
-      const parsedData = JSON.parse(storedData);
-      setDataFilters(parsedData);
-      parsedData.Jours.forEach((jour) => {
-        if (!jour.checked) setAllChecked(false);
-      });
-      parsedData.Scenes.forEach((scene) => {
-        if (!scene.checked) setAllChecked(false);
-      });
-      parsedData.Heures.forEach((heure) => {
-        if (!heure.checked) setAllChecked(false);
-      });
-    }
-  }, []);
+  
 
   const handleModalDisplay = () => {
     setModalIsOpen(!modalIsOpen);
   };
 
-  const handleCheck = (nomCategorie, id) => {
-    const updatedFilters = { ...dataFilters };
-    const categorie = updatedFilters[nomCategorie];
 
-    const index = categorie.findIndex((item) => item.id === id);
-
-    if (index !== -1) {
-      categorie[index].checked = !categorie[index].checked;
+  const handleAllGroupes = (e) => {
+    if (e.target.checked) {
+      setDataFilter([]);
+      setIsChecked(true);
+    } else {
+      setIsChecked(false);
     }
-
-    setDataFilters(updatedFilters);
-    console.log(updatedFilters);
-    localStorage.setItem("selectedCheckBox", JSON.stringify(updatedFilters));
-
-    const checkedFiltersNumber = Object.values(updatedFilters)
-      .flat()
-      .filter((filter) => filter.checked).length;
-
-    setAllChecked(
-      checkedFiltersNumber === 0 || checkedFiltersNumber === filtersNumber
-    );
   };
 
   return (
     <div className="p-10 flex flex-col items-center">
       <button
         onClick={handleModalDisplay}
-        className={`bg-[#023E33] text-white w-[15em] p-2 rounded-lg flex flex-1 justify-center text-[1.3rem] gap-2 z-30 ${
+        className={`bg-[#023E33] text-white w-[100%] p-2 rounded-lg flex flex-1 justify-center text-[1.3rem] gap-2 z-30 ${
           modalIsOpen ? "rounded-b-none" : ""
         }`}
       >
@@ -93,54 +52,30 @@ export const Filtrage = () => {
             : "-translate-y-full opacity-0 max-h-0"
         }`}
       >
-        <div className="flex flex-col p-5">
-          <div className="flex mb-10">
-            <CheckBoxTest
-              label="Tous"
-              id="tous"
-              handleCheck={() => {
-                setDataFilters(getDataFilters());
-                setAllChecked(true);
-              }}
-              checked={allChecked}
-            />
-          </div>
-          <div className="flex flex-wrap gap-[4em]">
-            {Object.keys(dataFilters).map((key) => (
-              <FiltrageCategorie
-                key={key}
-                nomCategorie={key}
-                filtres={dataFilters[key]}
-                handleCheck={handleCheck}
-              />
-            ))}
-          </div>
+      <div className="flex flex-col sm:flex-row sm:gap-5">
+        <div className="mb-2">
+          <input type="checkbox" id="allGroupes" name="allGroupes" onChange={handleAllGroupes} checked={isChecked}/>
+          <label htmlFor="allGroupes">Tous</label>
         </div>
+          <div className="flex flex-col gap-3 sm:flex-row sm:gap-[3em]">
+            <div>
+              <h3>Jour</h3>
+              <CheckBox data={uniqueDate} setDataFilter={setDataFilter} dataFilter={dataFilter} setIsChecked={setIsChecked}/>
+            </div>
+            <div>
+              <h3>Heure</h3>
+            <CheckBox data={uniqueHour} setDataFilter={setDataFilter} dataFilter={dataFilter} setIsChecked={setIsChecked} />
+            </div>
+            <div>
+              <h3>Scène</h3>
+              <CheckBox data={uniqueScene} setDataFilter={setDataFilter} dataFilter={dataFilter} setIsChecked={setIsChecked}/>
+            </div>
+        </div>
+        
+      </div>
       </div>
     </div>
   );
 };
 
-const FiltrageCategorie = ({ nomCategorie, filtres, handleCheck }) => {
-  return (
-    <div className="">
-      <h2 className=" font-bold text-[1.5rem] mb-3">{nomCategorie}</h2>
-      <ul>
-        {filtres.map((filtre) => (
-          <li
-            key={filtre.id}
-            className="flex flex-row justify-between gap-3 mb-3 "
-          >
-            <CheckBoxTest
-              label={filtre.label}
-              id={filtre.id}
-              handleCheck={handleCheck}
-              nomCategorie={nomCategorie}
-              checked={filtre.checked}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+
--- Sound-Nation/src/pages/programmation.jsx
+++ Sound-Nation/src/pages/programmation.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from "react";
 import { AffichageGroupe } from "../components/AffichageGroupe";
 import { Filtrage } from "../components/Filtrage";
 import { Layout } from "../components/Layout";
-import { FilterProvider } from "../contexts/FilterContext";
 import axios from "axios";
-
+// Page de la programmation
 export const Programmation = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState(null);
-  const [uniqueCat, setUniqueCat] = useState([]);
+  const [data, setData] = useState([]);
+  const [uniqueDate, setUniqueDate] = useState([]);
+  const [uniqueHour, setUniqueHour] = useState([]);
+  const [uniqueScene, setUniqueScene] = useState([]);
   const [message, setMessage] = useState("");
+  const [dataFilter, setDataFilter] = useState([]);
+  const [isChecked, setIsChecked] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
@@ -17,11 +20,17 @@ export const Programmation = () => {
         const response = await axios.get('http://localhost:3000/api/groupes');
         if(response.data.status){
           setData(response.data.data);
-          const tempCat =[];
+          const tempDate =[];
+          const tempHour =[];
+          const tempScene =[];
           response.data.data.map((groupe)=>{
-            tempCat.push(groupe.groupe_category);
+            tempDate.push(groupe.groupe_date);
+            tempHour.push(groupe.groupe_hour);
+            tempScene.push(groupe.groupe_scene);
           })
-          setUniqueCat([...new Set(tempCat)]);
+          setUniqueDate([...new Set(tempDate.reverse())]);
+          setUniqueHour([...new Set(tempHour.sort((a, b)=>a-b))]);
+          setUniqueScene([...new Set(tempScene.sort((a, b)=>a-b))]);
           setIsLoading(false);
         } else{
           setMessage(response.data.message);
@@ -35,6 +44,26 @@ export const Programmation = () => {
     getData();
   }, []);
 
+  const dataToDisplay = data.filter((groupe) => {
+    if (dataFilter.length === 0) {
+      return true;
+    }
+  
+    const dateFilters = dataFilter.filter((filter) => uniqueDate.includes(filter));
+    const hourFilters = dataFilter.filter((filter) => uniqueHour.includes(parseInt(filter)));
+    const sceneFilters = dataFilter.filter((filter) => uniqueScene.includes(parseInt(filter)));
+  
+    const matchesDate = dateFilters.length === 0 || dateFilters.includes(groupe.groupe_date);
+    const matchesHour = hourFilters.length === 0 || hourFilters.includes(groupe.groupe_hour.toString());
+    const matchesScene = sceneFilters.length === 0 || sceneFilters.includes(groupe.groupe_scene.toString());
+  
+    return matchesDate && matchesHour && matchesScene;
+  });
+  
+  
+  
+  
+
 
 
   if (isLoading) {
@@ -45,12 +74,10 @@ export const Programmation = () => {
         <div className="bg-black m-10 rounded-lg flex flex-col items-center mb-10">
           <h1 className="text-white flex justify-center p-5 mb-5 text-[2rem]">
             Programmation
-            {console.log(data)}
+            {console.log(dataToDisplay)}
           </h1>
-          {/* <FilterProvider> */}
-            {/* <Filtrage /> */}
-            <AffichageGroupe data={data} />
-          {/* </FilterProvider> */}
+            <Filtrage uniqueDate={uniqueDate} uniqueHour={uniqueHour} uniqueScene={uniqueScene} setDataFilter={setDataFilter} dataFilter={dataFilter} isChecked={isChecked} setIsChecked={setIsChecked}  />
+            <AffichageGroupe data={dataToDisplay} />
         </div>
       </Layout>
     );
